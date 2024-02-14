2d59ef0fd07816cd32c4ae418f9b59ad4af02327
aimthecoder <ahmedismailm85@gmail.com>
2024-02-14 19:39:12 +0200

    feature: test for different type of employee leaves

---
+++ test.py
@@ -0,0 +1,26 @@
+import unittest
+from models.employee import EMPLOYEE
+from models.reasons import JobLeaveReasons
+
+
+class TestIndemnity(unittest.TestCase):
+    def setUp(self):
+        self.test_cases = [
+            #JOING DAT      # LEAVING REASON                #SALARY
+            #(DD-MM-YYYY)     Choices                        FLOAT
+            ("01-2-2023", JobLeaveReasons.RESIGNATION.value, 0),
+            ("01-2-2022", JobLeaveReasons.RESIGNATION.value, 1666.67),
+            ("01-2-2018", JobLeaveReasons.RESIGNATION.value, 11666.67),
+            ("01-7-2017", JobLeaveReasons.RESIGNATION.value, 13333.33),
+            ("01-2-2014", JobLeaveReasons.RESIGNATION.value, 37500),
+        ]
+
+    def test_calculate_indemnity(self):
+        for joining_date, reason, expected_indemnity in self.test_cases:
+            self.example_employee = EMPLOYEE(base_salary=5000, joining_date=joining_date)
+            actual_indemnity = self.example_employee.calculate_indemnity(reason=reason)
+            self.assertEqual(actual_indemnity, expected_indemnity)
+
+
+if __name__ == "__main__":
+    unittest.main()
