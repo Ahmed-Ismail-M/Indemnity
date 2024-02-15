56db048163b4a3e2a6e91f17670280c50b22b972
aimthecoder <ahmedismailm85@gmail.com>
2024-02-15 06:15:15 +0200

    fixed: duplication of leave rules

--- ds/local.py
+++ ds/local.py
@@ -1,13 +1,12 @@
 from models.reasons import JobLeaveOptions, JobLeaveReasons
 KSAJobLeaveRules = {
-    JobLeaveReasons.AGAINST_ARTICLE_81.value: JobLeaveOptions.COMPLETE,
     JobLeaveReasons.MATERNITY.value: JobLeaveOptions.COMPLETE,
     JobLeaveReasons.AGREEMAENT.value: JobLeaveOptions.COMPLETE,
     JobLeaveReasons.RESIGNATION.value: JobLeaveOptions.BASED_ON_EXPERIENCE,
     JobLeaveReasons.AGAINST_ARTICLE_81.value: JobLeaveOptions.NA,
     JobLeaveReasons.FORCE_MAJEURE.value: JobLeaveOptions.COMPLETE,
     JobLeaveReasons.TERMINATION.value: JobLeaveOptions.COMPLETE,
-    JobLeaveReasons.WITH_ARTCLE_80.value: JobLeaveOptions.COMPLETE,
+    JobLeaveReasons.WITH_ARTCLE_80.value: JobLeaveOptions.NA,
     JobLeaveReasons.WITH_ARTICLE_81.value: JobLeaveOptions.COMPLETE
 }
 
